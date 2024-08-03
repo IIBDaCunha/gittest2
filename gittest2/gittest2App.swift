491df4631ca94739024540f93805623f0db2e0f9
IIBDaCunha <tristantan118@gmail.com>
2024-08-03 09:39:05 +0800

    Initial Commit

---
+++ gittest2/gittest2App.swift
@@ -0,0 +1,17 @@
+//
+//  gittest2App.swift
+//  gittest2
+//
+//  Created by Student on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct gittest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gittest2UITests/gittest2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  gittest2UITestsLaunchTests.swift
+//  gittest2UITests
+//
+//  Created by Student on 3/8/24.
+//
+
+import XCTest
+
+final class gittest2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
